a0ad56dd1a9a858a6e84fbca4836296098738fcb
ajlan-pk <ajlan0300@gmail.com>
2026-09-13 22:19:59 +0500

    Add files via upload

---
+++ restaurants management systems .c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+int main() {
+	
+
+int choice;    
+do{	    
+printf("\n========================================= WELCOME TO BEAST RESTAURANT========================================\n1. View Food Menu\n2. Order Food\n3. View Cart\n4. Remove Item\n5. Generate Bill\n6. Exit\nEnter Choice:");
+scanf("%d",&choice);
+switch(choice){
+ case 1:
+    printf("1: Burger        Rs.450  \n2 :Pizza         Rs.1200 \n3: Zinger Burger Rs.650   \n4: Fries         Rs.300 \n5: Shawarma      Rs.250\n6: Sandwich      Rs.500\n7: Pasta         Rs.850\n8: Coffee        Rs.280\n9: Cold Drink    Rs.180\n10: Ice Cream    Rs.350\n");
+    break;
+ case 2:
+ 	printf("========== ORDER FOOD ==========\nEnter Food Number:");
+ 	int FoodChoice;
+ 	scanf("%d",&FoodChoice);
+ 	int quantity;
+ 	printf("\nEnter Quantity:");
+ 	scanf("%d",&quantity);
+ 	int price;
+    int totalprice; 
+ 	switch(FoodChoice){
+ 		case 1:
+ 			printf("\nYou Selected burger\n");
+ 			price=450;
+ 			totalprice=price*quantity;
+            printf("Price: Rs.%d\n", price);
+printf("Quantity:%d\n", quantity);
+printf("Total Price: Rs.%d\n", totalprice);
+ 			break;
+ 			case 2:
+ 				printf("You Selected Pizza\n");
+ 				price=1200;
+ 				totalprice=price*quantity;
+ 				printf("Price: Rs.%d\n", price);
+printf("Quantity:%d\n", quantity);
+printf("Total Price: Rs.%d\n", totalprice);
+ 				break;
+ 				case 3:
+ 					printf("You Selected Zinger Burger\n");
+ 					price=650;
+ 					totalprice=price*quantity;
+ 					printf("Price: Rs.%d\n", price);
+printf("Quantity:%d\n", quantity);
+printf("Total Price: Rs.%d\n", totalprice);
+ 					break;
+ 					case 4:
+ 						printf("You Selected Fries\n");
+ 						price=300;
+ 						totalprice=price*quantity;
+ 						printf("Price: Rs.%d\n", price);
+printf("Quantity:%d\n", quantity);
+printf("Total Price: Rs.%d\n", totalprice);
+ 						break;
+ 						case 5:
+ 							printf("You Selected Shawarma\n");
+ 							price=250;
+ 							totalprice=price*quantity;
+ 							printf("Price: Rs.%d\n", price);
+printf("Quantity:%d\n", quantity);
+printf("Total Price: Rs.%d\n", totalprice);
+ 							break;
+ 							case 6:
+ 								printf("You Selected Sandwich\n");
+ 								price=500;
+ 								totalprice=price*quantity;
+ 								printf("Price: Rs.%d\n", price);
+printf("Quantity:%d\n", quantity);
+printf("Total Price: Rs.%d\n", totalprice);
+ 								break;
+ 								case 7:
+ 									printf("You Selected Pasta\n");
+ 									price=850;
+ 									totalprice=price*quantity;
+ 									printf("Price: Rs.%d\n", price);
+printf("Quantity:%d\n", quantity);
+printf("Total Price: Rs.%d\n", totalprice);
+ 									break;
+ 									case 8:
+ 										printf("You Selected Coffee\n");
+ 										price=280;
+ 										totalprice=price*quantity;
+ 										printf("Price: Rs.%d\n", price);
+printf("Quantity:%d\n", quantity);
+printf("Total Price: Rs.%d\n", totalprice);
+ 										break;
+ 										case 9:
+ 											printf("You Selected Cold Drink\n");
+ 											price=180;
+ 											totalprice=price*quantity;printf("Price: Rs.%d\n", price);
+printf("Quantity:%d\n", quantity);
+printf("Total Price: Rs.%d\n", totalprice);
+ 											
+ 											break;
+ 											case 10:
+ 												printf("You Selected Ice Cream\n");
+ 												price=350;
+ 												totalprice=price*quantity;
+ 												printf("Price: Rs.%d\n", price);
+printf("Quantity:%d\n", quantity);
+printf("Total Price: Rs.%d\n", totalprice);
+ 												break;
+ 												default:
+ 													printf("Invalid Food Choice\n");
+	 }
+ 	break;
+ case 3:
+    printf("View Cart Selected\n");
+    break;
+ case 4:
+    printf("Remove Item Selected\n");
+    break;
+ case 5:
+    printf("Generate Bill Selected\n");
+    break;
+ case 6:
+    printf("Thank You! Visit Again.\n");
+    break;
+ default:		  
+    printf("Invalid Choice\n"); 
+    
+}
+
+}
+while(choice!=6);
+
+return 0;
+}
